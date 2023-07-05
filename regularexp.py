b37a38d550b851d6c0b4966ba97130a48dfa4c6e
DarpanN <cloversdnc1994@gmail.com>
2023-07-05 21:34:58 +0545

    split str

--- regularexp.py
+++ regularexp.py
@@ -1,12 +1,18 @@
 import re
 
-content = "Hello!! Boss Welcome To To My World"
-word = "Boss"
+content = "Hello!! Boss Welcome To My World"
+word = "\s"
 
 def findWord(word, str):
     content = re.findall(word, str)
     print(content)
 
 findWord(word, content)
+
+def splitStr(word, str):
+    content=re.split(word, str)
+    print(content)
+
+splitStr(word, content)
     
     
